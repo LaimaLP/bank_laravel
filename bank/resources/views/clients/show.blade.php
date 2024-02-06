611300cdd3d9d4b7f2846094b3786b45e12cbc55
LaimaLP <pileliene.laima@gmail.com>
2024-02-06 22:52:31 +0200

    client list background style

--- bank/resources/views/clients/show.blade.php
+++ bank/resources/views/clients/show.blade.php
@@ -10,13 +10,17 @@
                     <div class="card-body">
                         @if ($client->accounts()->count() > 0)
                         <div class="card-body">
-                            {{-- cia duoda kolekcija su visai truckais. o jei uzdesime (), pasiimam priklausomybe ir joje paskaiciuoti. Duomenu bazes uzklausos countas ...  --}}
                             <ul class="list-group list-group-flush">
                                 @foreach ($client->accounts as $account)
+
                                 <a class="list-group-item" href={{ route('accounts-show', $account) }} >{{ $account->accountNumber }} : <b>{{ $account->balance }} € </b></a>
+
+
+                                <p>  Total balance in {{$client->name}} {{$client->surname}} accounts: </p>
                                 @endforeach
                             </ul>
-                            <ul class="list-group-item"> Total balance in {{$client->name}} {{$client->surname}} accounts:  </ul>
+                            <ul class="list-group-item">  
+                                {{$account->balance}}  </ul>
                         </div>
                         @endif
                     </td>
