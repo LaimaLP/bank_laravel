e3380f920483aca7c583db36ca9d40239663eeb4
LaimaLP <pileliene.laima@gmail.com>
2024-02-06 22:51:13 +0200

    statistics

--- bank/app/Http/Controllers/HomeController.php
+++ bank/app/Http/Controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Client;
+use App\Models\Account;
 
 use Illuminate\Http\Request;
 
@@ -24,9 +26,27 @@ class HomeController extends Controller
     public function index()
     {
 
+        $accountsCount = Account::count();
+        $clientsCount = Client::count();
+        $totalBalance = Account::sum('balance');
+        $balanceAverage = Account::avg('balance');
 
+        $accountWithMaxBalance = Account::select('id', 'balance')->orderBy('balance', 'desc' )->first();
+
+        $accountsWithZeroBalance = Account::where('balance', 0)->count();
+        $accountsWithNegativeBalanceCount = Account::where('balance', '<', 0)->count();
+
+        return view('home', [
+            'accountsCount' => $accountsCount,
+            'clientsCount' => $clientsCount,
+            'accountWithMaxBalance' => $accountWithMaxBalance,
+            'totalBalance'=>$totalBalance,
+            'balanceAverage'=>$balanceAverage,
+            'accountsWithZeroBalance'=> $accountsWithZeroBalance,
+            'accountsWithNegativeBalanceCount'=> $accountsWithNegativeBalanceCount,
+        ]);
 
         
-        return view('home');
+      
     }
 }
